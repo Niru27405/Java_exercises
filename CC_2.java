ee4b780cf18a7dc731ef37b44c32ca1603d021a6
Niru27405 <niranjanaravi2745@gmail.com>
2024-12-23 21:18:16 +0530

    Modified CC_2

--- CC_2.java
+++ CC_2.java
@@ -5,13 +5,14 @@ class CC_2{
     {
         // get 3 inputs,add all and store in e , multiply all and store in e ,now divide d by e.
         Scanner john = new Scanner(System.in);
-        // int a = john.nextInt();
-        // int b = john.nextInt();
-        // int c = john.nextInt();
-        // int d = a*b*c;
-        // int e = a+b+c;
-        // System.out.print(d/e);
+        int a = john.nextInt();
+        int b = john.nextInt();
+        int c = john.nextInt();
+        int d = a*b*c;
+        int e = a+b+c;
+        System.out.print(d/e);
 
+        // get name,score,dept and convert score out of 10.
         String Name = john.nextLine();
         double Score = john.nextDouble();
         john.nextLine();
