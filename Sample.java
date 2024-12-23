bec3b7b068f49db29b813bf612e71c79f87e61ae
Niru27405 <niranjanaravi2745@gmail.com>
2024-12-23 20:55:57 +0530

    Created Sample.Java

---
+++ Sample.java
@@ -0,0 +1,18 @@
+
+class Datatypes_variables{
+    public static void main(String args[])
+    {
+      int container = 67;
+       System.out.print(container);
+       String box = "John";
+       System.out.print(box);
+
+       // program to add 3 numbers
+
+       int a = 10;
+       int b = 4;
+       int c = 8;
+       int d = a+b+c;
+       System.out.print(d);
+    }
+}
