d32b55df8b3ffe1363d3d5c5701d5bfbadc2b5f3
Niru27405 <niranjanaravi2745@gmail.com>
2024-12-23 21:15:23 +0530

    Created CC_2

---
+++ CC_2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class CC_2{
+    public static void main(String args[])
+    {
+        // get 3 inputs,add all and store in e , multiply all and store in e ,now divide d by e.
+        Scanner john = new Scanner(System.in);
+        // int a = john.nextInt();
+        // int b = john.nextInt();
+        // int c = john.nextInt();
+        // int d = a*b*c;
+        // int e = a+b+c;
+        // System.out.print(d/e);
+
+        String Name = john.nextLine();
+        double Score = john.nextDouble();
+        john.nextLine();
+        String Department = john.nextLine();
+       
+        System.out.print(Score/10);
+    }
+}
